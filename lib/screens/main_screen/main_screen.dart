bf2c1b382aaba90dfadd330a972af5a0c93240d9
Mohammed Samy <mom118580@gmail.com>
2025-06-20 09:30:23 +0300

    Colored Svg Picture

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -22,17 +22,17 @@ class _MainScreenState extends State<MainScreen> {
     ProfileScreen(),
   ];
 
-  int _currentScreen = 0;
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentScreen,
+        currentIndex: _currentIndex,
         onTap: (int? index) {
           log(index.toString());
           setState(() {
-            _currentScreen = index ?? 0;
+            _currentIndex = index ?? 0;
           });
         },
         backgroundColor: Color(0xFF181818),
@@ -41,24 +41,48 @@ class _MainScreenState extends State<MainScreen> {
         selectedItemColor: Color(0xff15B86C),
         items: [
           BottomNavigationBarItem(
-            icon: SvgPicture.asset("assets/images/home.svg"),
+            icon: SvgPicture.asset(
+              "assets/images/home.svg",
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 0 ? Color(0xff15B86C) : Color(0xffC6C6C6),
+                BlendMode.srcIn,
+              ),
+            ),
             label: "Home",
           ),
           BottomNavigationBarItem(
-            icon: SvgPicture.asset("assets/images/todo.svg"),
+            icon: SvgPicture.asset(
+              "assets/images/todo.svg",
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 1 ? Color(0xff15B86C) : Color(0xffC6C6C6),
+                BlendMode.srcIn,
+              ),
+            ),
             label: "ToDo",
           ),
           BottomNavigationBarItem(
-            icon: SvgPicture.asset("assets/images/completed.svg"),
+            icon: SvgPicture.asset(
+              "assets/images/completed.svg",
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 2 ? Color(0xff15B86C) : Color(0xffC6C6C6),
+                BlendMode.srcIn,
+              ),
+            ),
             label: "completed",
           ),
           BottomNavigationBarItem(
-            icon: SvgPicture.asset("assets/images/profile.svg"),
+            icon: SvgPicture.asset(
+              "assets/images/profile.svg",
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 3 ? Color(0xff15B86C) : Color(0xffC6C6C6),
+                BlendMode.srcIn,
+              ),
+            ),
             label: "profile",
           ),
         ],
       ),
-      body: screens[_currentScreen],
+      body: screens[_currentIndex],
     );
   }
 }
