68921413ffbee6691fe7915999aaa6fd6512090b
Mohammed Samy <mom118580@gmail.com>
2025-06-21 07:40:34 +0300

    create Completed Tasks screen

--- lib/screens/complete_tasks_screen/complete_tasks_screen.dart
+++ lib/screens/complete_tasks_screen/complete_tasks_screen.dart
@@ -1,16 +1,96 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-class CompleteTasksScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tasky/models/task_model.dart';
+import 'package:tasky/widgets/tasks_list_widget.dart';
+
+class CompleteTasksScreen extends StatefulWidget {
   const CompleteTasksScreen({super.key});
+
+  @override
+  State<CompleteTasksScreen> createState() => _CompleteTasksScreenState();
+}
+
+class _CompleteTasksScreenState extends State<CompleteTasksScreen> {
+  List<TaskModel> tasks = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTask();
+  }
+
+  void _loadTask() async {
+    setState(() {
+      isLoading = true;
+    });
+    final pref = await SharedPreferences.getInstance();
+    final finalTask = pref.getString("tasks");
+
+    if (finalTask != null) {
+      final taskAfterDecode = jsonDecode(finalTask) as List<dynamic>;
+
+      setState(() {
+        // task = taskAfterDecode;
+        tasks =
+            taskAfterDecode
+                .map((element) => TaskModel.fromJson(element))
+                .where((element) => element.isDone)
+                .toList();
+        // Short for this code
+        // tasks = taskAfterDecode.map((e) => TaskModel.fromJson(e)).toList();
+        // tasks = tasks.where((element) => element.isDone == false).toList();
+      });
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> _saveTasksToPrefs() async {
+    try {
+      final pref = await SharedPreferences.getInstance();
+      final updatedTasks = tasks.map((e) => e.toMap()).toList();
+      final valueEncode = jsonEncode(updatedTasks);
+      await pref.setString("tasks", valueEncode);
+      log("✅ Tasks saved successfully.");
+    } catch (e) {
+      log("❌ Error saving tasks: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "completed Screen",
-          style: TextStyle(color: Colors.red, fontSize: 50),
-        ),
+      appBar: AppBar(title: Text("Completed Tasks")),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child:
+            isLoading
+                ? Center(
+                  child: CircularProgressIndicator(color: Color(0xffFFFCFC)),
+                )
+                : tasks.isEmpty
+                ? Center(
+                  child: Text(
+                    "No Task Found",
+                    style: TextStyle(color: Color(0xffFFFCFC), fontSize: 24),
+                  ),
+                )
+                : TasksListWidget(
+                  tasks: tasks,
+                  onTap: (bool? value, int? index) async {
+                    setState(() {
+                      tasks[index!].isDone = value ?? false;
+                    });
+                    await _saveTasksToPrefs();
+                    _loadTask();
+                  },
+                ),
       ),
     );
   }
